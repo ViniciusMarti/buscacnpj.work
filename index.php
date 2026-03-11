021665d55da5ee593a9a54691a77414a6f653ae4
Vinicius Martins Oliveira <vinicius0102m@gmail.com>
2026-03-11 15:20:53 -0300

    Integrate local FontAwesome library

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     
     <!-- CSS & Fonts -->
     <link rel="stylesheet" href="/assets/cnpj.css?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/assets/cnpj.css'); ?>">
+    <link href="/fontawesome/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     
     <style>
@@ -90,9 +91,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <div class="header-inner">
         <a class="logo" href="/" aria-label="BuscaCNPJ Grátis - Ir para a página inicial">Busca<span>CNPJ</span> Grátis</a>
         <nav>
-            <a href="/">Início</a>
-            <a href="/rankings/">Rankings</a>
-            <a href="/sobre/">Sobre</a>
+            <a href="/"><i class="fa-solid fa-house mr-1"></i> Início</a>
+            <a href="/rankings/"><i class="fa-solid fa-chart-simple mr-1"></i> Rankings</a>
+            <a href="/sobre/"><i class="fa-solid fa-circle-info mr-1"></i> Sobre</a>
         </nav>
     </div>
 </header>
@@ -105,7 +106,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="search-container">
             <input id="q" type="text" maxlength="18" placeholder="Digite o CNPJ (apenas números)..." 
                    onkeydown="if(event.key==='Enter')buscar()" aria-label="Digite o CNPJ para consulta" autofocus>
-            <button onclick="buscar()">Consultar</button>
+            <button onclick="buscar()"><i class="fa-solid fa-magnifying-glass mr-2"></i> Consultar</button>
         </div>
     </div>
 
